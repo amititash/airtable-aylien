c48cae3758abb3403760023c735c6f1e5a013d19
Titash Neogi <neogi.titash@gmail.com>
2019-08-29 10:16:26 +0530

    changed routes for gigsforme

--- app.js
+++ app.js
@@ -1,102 +1,100 @@
 require('dotenv').config();
 
 const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.API_KEY}).base('appyA5vxxYmLCEW8t');
+const base = new Airtable({apiKey: process.env.API_KEY}).base('appnMFTR9pdwCgCzF');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const uuidv1 = require('uuid/v1');
 
-//const summaryEngine = require('./summaryengine.js');
-const summaryEngine = require('./aylienSummary.js');
-const categoryEngine = require('./aylienConcept.js');
-const imageEngine = require('./aylienImageExtract.js');
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.post('/airtablecreate', (req, res) => {
-
-    console.log(req.body);
-    
-    base('MainFeed').create({
-        "ArticleTitle": req.body.title,
-        "ArticleContent": req.body.content,
-        "ArticleURL": req.body.url,
-        "ArticleCategories": req.body.categories,
-        "SourceTitle": req.body.sourceTitle,
-        "SourceURL": req.body.sourceURL,
-        "ArticleFirstImageURL": req.body.articleImage,
-    }, function(err, record) {
-        if (err) { 
-            res.status(500).json({
-                success: false,
-                err: err.message
-            })    
-        }
-        console.log(record.getId());
-        res.status(200).json({
-            success: true,
-            id: record.getId()
-        })
-    });
-});
-
-app.get('/airtablesummarise',(req, res) => {
-
-    var recordSet = [];
-
-    base('MainFeed').select({
-        // Selecting the first 3 records in Grid view:
-        //maxRecords: 10,
-        pageSize: 100,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-    
-        records.forEach(function(record) {
-            console.log('Processing ... ', record.id);
-
-            // fetch each record and send it to queue for further processing 
-           // summaryEngine('Fetch_Data', record.id);
-            categoryEngine('Fetch_Category', record.id);
-           // imageEngine('Fetch_Image', record.id);
-        });
-    
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        //fetchNextPage();
-    
-    }, function done(err) {
-        if (err) { console.error(err); 
-            res.send("err") }
-        else { res.send("job sent");}
-    });
-});
-
 app.get('/airtablefetchpage',(req, res) => {
 
     var recordSet = [];
+    var chosenSkill = req.query.chosenskill;
+    console.log("fetching "+chosenSkill);
 
-base('MainFeed').select({
+base('MasterList').select({
         view: 'Grid view'
     }).firstPage(function(err, records) {
         if (err) { console.error(err); return; }
         records.forEach(function(record) {
-            console.log('Retrieved', record.get('Id'));
+            //console.log('Retrieved', record.get('Id'));
 
-            var result = {
-                artTitle : record.fields.ArticleTitle,
-                shortSummari : record.fields.ShortSummary,
-                longSummari: record.fields.LongSummary,
-                artUrl : record.fields.ArticleURL,
-                srcTitle : record.fields.sourceTitle,
-                articleFirstImgURL : record.fields.ArticleFirstImageURL
+            if(record.fields.Skills)
+            {
+
+                var skillArr = record.fields.Skills;
+                //console.log(skillArr);
+    
+                if (skillArr.includes(chosenSkill))
+                {
+    
+                    var result = {
+                        id:uuidv1(),
+                        sitename : record.fields.Sitename,
+                        sitetype : record.fields.SiteType,
+                        desc: record.fields.Description,
+                        url : record.fields.URL,
+                        usagetype : record.fields.Usagetype,
+                        skills : record.fields.Skills,
+                        country : record.fields.Country,
+                        logo: record.fields.Logo[0]["url"]
+                    }
+                    recordSet.push(result);
+    
+                }
+                else
+                {
+                    //console.log("skip");
+                }
+
             }
-            recordSet.push(result);
+           
+
+            
+        });
+		res.json(recordSet);
+    });
+});
+
+app.get('/getskills',(req, res) => {
+
+    var recordSet = [];
+    console.log("fetching");
+
+base('MasterList').select({
+        view: 'Grid view'
+    }).firstPage(function(err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(function(record) {
+            //console.log('Retrieved', record.get('Id'));
+
+            if(record.fields.Skills)
+            {
+
+                var skillArr = record.fields.Skills;
+                //console.log(skillArr);
+    
+                    skillArr.forEach(function(skill){
+
+                        if(!recordSet.includes(skill))
+                        {
+                            recordSet.push(skill);
+                        }
+                    })
+                  
+            }
+           
+
+            
         });
 		res.json(recordSet);
     });
