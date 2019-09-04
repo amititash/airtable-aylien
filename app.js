0d3a4872b63e8b3dfbb2342ccccd4ec557b343dc
Titash Neogi <neogi.titash@gmail.com>
2019-09-04 17:22:01 +0530

    pagination issue resolved

--- app.js
+++ app.js
@@ -14,29 +14,26 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-app.get('/airtablefetchpage',(req, res) => {
+app.get('/getplatforms', (req, res) => {
 
     var recordSet = [];
     var chosenSkill = req.query.chosenskill;
     console.log("fetching "+chosenSkill);
 
-base('MasterList').select({
-        view: 'Grid view'
-    }).firstPage(function(err, records) {
-        if (err) { console.error(err); return; }
+    base('MasterList').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 200,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
         records.forEach(function(record) {
             //console.log('Retrieved', record.get('Id'));
-
             if(record.fields.Skills)
             {
-
                 var skillArr = record.fields.Skills;
                 //console.log(skillArr);
-    
                 if (skillArr.includes(chosenSkill))
                 {
-    
                     var result = {
                         id:uuidv1(),
                         sitename : record.fields.Sitename,
@@ -49,23 +46,87 @@ base('MasterList').select({
                         logo: record.fields.Logo[0]["url"]
                     }
                     recordSet.push(result);
-    
                 }
                 else
                 {
                     //console.log("skip");
                 }
+            }
+            
+        });
+		
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+        else{
+            res.json(recordSet);
+    
+        }
+    });
+    
 
+
+})
+
+
+app.get('/getskills', (req, res) => {
+
+    var recordSet = [];
+    var chosenSkill = req.query.chosenskill;
+    console.log("fetching "+chosenSkill);
+
+    base('MasterList').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 200,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        records.forEach(function(record) {
+            //console.log('Retrieved', record.get('Id'));
+
+            if(record.fields.Skills)
+            {
+
+                var skillArr = record.fields.Skills;
+                //console.log(skillArr);
+    
+                    skillArr.forEach(function(skill){
+
+                        if(!recordSet.includes(skill))
+                        {
+                            recordSet.push(skill);
+                        }
+                    })
+                  
             }
            
 
             
         });
-		res.json(recordSet);
+		
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+        else{
+            res.json(recordSet);
+    
+        }
     });
-});
+    
 
-app.get('/getskills',(req, res) => {
+
+})
+
+
+app.get('/getskillsold',(req, res) => {
 
     var recordSet = [];
     console.log("fetching");
